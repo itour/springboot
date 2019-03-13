91cb6f9aa1e1ef3f20df5e0ddf03ee31ad1419a3
Ray <wti-itour@hotmail.com>
2019-03-13 17:42:31 +0800

    zuul sample

---
+++ zuul/src/main/java/wti/itour/cn/Application.java
@@ -0,0 +1,22 @@
+package wti.itour.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Ray
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@RefreshScope
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
